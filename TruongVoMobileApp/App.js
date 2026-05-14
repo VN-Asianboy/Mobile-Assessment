8997a3ea408aa72f82a27c6b6e38e468b7ab713a
An Vo <35384824+VN-Asianboy@users.noreply.github.com>
2026-05-14 15:54:22 +1000

    sadasd

--- TruongVoMobileApp/App.js
+++ TruongVoMobileApp/App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import AnotherPage from './screens/AnotherPage';
+import YetAnotherPage from './screens/YetAnotherPage';
+import styles from './styles';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={({ navigation }) => ({
+          headerStyle: styles.header,
+          headerTitleStyle: styles.headerTitle,
+        })}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="AnotherPage" component={AnotherPage} />
+        <Stack.Screen name="YetAnotherPage" component={YetAnotherPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
